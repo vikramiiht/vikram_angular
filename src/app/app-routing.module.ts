617d6595fedeada13de635e0a680604b2903220d
DESKTOP-FPS80CE\Administrator <vikram.kumar@iiht.com>
2021-02-24 13:04:39 +0530

    routing banding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component'
+import { AdminComponent } from './admin/admin.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'admin',
+    component:AdminComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
